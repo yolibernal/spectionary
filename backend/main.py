6ab92d04df04542623d6d60f49060308dda37bf5
Jonas Bernhard <jonasbernhard96@gmail.com>
2022-05-13 12:46:00 +0200

    Add game rooms with message broadcasting

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import json
 import uuid
 from datetime import datetime
-from typing import List
+from typing import Dict, List, Optional
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
@@ -24,51 +24,96 @@ class CreateRoomModel(BaseModel):
 
 
 class GameRoom:
+    def __init__(self, room_id: str):
+        self.room_id = room_id
+        self.client_ids = []
+
+    def add_client(self, client_id: str):
+        if client_id in self.client_ids:
+            return
+        self.client_ids.append(client_id)
+
+    async def broadcast(self, message: str):
+        await connection_manager.broadcast_to_clients(self.client_ids, message)
+
+
+class GameRoomManager:
+    """
+    Manages the game rooms.
+    """
+
     def __init__(self):
-        self.room_id = str(uuid.uuid4())
+        self.rooms = {}
+
+    def create_room(self) -> str:
+        room_id = str(uuid.uuid4())
+        game_room = GameRoom(room_id=room_id)
+        self.rooms[room_id] = game_room
+        return game_room
+
+    def get_room(self, room_id: str) -> Optional[GameRoom]:
+        return self.rooms.get(room_id, None)
+
+    def delete_room(self, room_id: str) -> None:
+        self.rooms.pop(room_id, None)
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.active_connections: Dict[str, WebSocket] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, client_id: str, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[client_id] = websocket
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, client_id: str):
+        websocket = self.active_connections.pop(client_id, None)
+        if websocket is not None:
+            websocket.close()
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
+    async def send_message_to_client(self, client_id: str, message: str):
+        websocket = self.active_connections.get(client_id, None)
+        if websocket is None:
+            return
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
+    async def broadcast_all(self, message: str):
+        for connection in self.active_connections.items():
             await connection.send_text(message)
 
+    async def broadcast_to_clients(self, client_ids: List[str], message: str):
+        for client_id in client_ids:
+            await self.send_message_to_client(client_id, message)
 
-manager = ConnectionManager()
+
+game_room_manager = GameRoomManager()
+connection_manager = ConnectionManager()
 
 
 @app.post("/create-room")
 async def create_room(data: CreateRoomModel):
-    game_room = GameRoom()
+    game_room = game_room_manager.create_room()
 
     return {"room_id": game_room.room_id}
 
 
 @app.websocket("/ws/{room_id}/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await manager.connect(websocket)
+async def websocket_endpoint(websocket: WebSocket, room_id: str, client_id: str):
     now = datetime.now()
     current_time = now.strftime("%H:%M")
+
+    game_room = game_room_manager.get_room(room_id)
+    game_room.add_client(client_id)
+
+    await connection_manager.connect(client_id, websocket)
+
     try:
         while True:
             data = await websocket.receive_json()
             message = {**data, "time": current_time}
-            await manager.broadcast(json.dumps(message))
+            await game_room.broadcast(json.dumps(message))
 
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
+        connection_manager.disconnect(client_id)
         message = {"time": current_time, "type": "disconnected", "message": "Offline"}
-        await manager.broadcast(json.dumps(message))
+        await game_room.broadcast(json.dumps(message))
