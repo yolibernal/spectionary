c713f0b720cb0f711709a7b2a325a9073519f95f
Jonas Bernhard <jonasbernhard96@gmail.com>
2022-05-13 20:53:50 +0200

    Persist client ID in local storage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,22 @@ import "./App.css"
 import { GameView } from "./components/GameView"
 import { JoinRoomView } from "./components/JoinRoomView"
 
+// https://www.robinwieruch.de/local-storage-react/#react-local-storage-hook
+const useLocalStorage = (storageKey: string, fallbackState: any) => {
+  const storedValue = localStorage.getItem(storageKey)
+  const [value, setValue] = React.useState(
+    storedValue ? JSON.parse(storedValue) : fallbackState
+  )
+
+  React.useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(value))
+  }, [value, storageKey])
+
+  return [value, setValue]
+}
+
 const App = () => {
-  const [myClientId, setMyClientId] = useState(uuidv4())
+  const [myClientId, setMyClientId] = useLocalStorage("clientId", uuidv4())
 
   const [roomId, setRoomId] = useState<string | null>(null)
   const [streamId, setStreamId] = useState<string | null>(null)
